5052d69c63d81625e364836247008e6ea19b0de2
anjolaoluwaakindipe <anjyakindipe@gmail.com>
2023-02-09 07:35:32 -0500

    Save before going to Uncle Yemi place

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,21 @@
-import LandingBanner from '../components/pages/Landing/LandingBanner';
-import { NextPageWithLayout } from './_app';
+import LandingBanner from "../components/pages/Landing/LandingBanner";
+import { NextPageWithLayout } from "./_app";
 
-import ExploreBestShow from '../components/pages/Landing/ExploreBestShow';
-import UpcomingEvents from '../components/pages/Landing/UpcomingEvents';
-import MostPopularTicket from '../components/pages/Landing/MostPopularTicket';
-import CheckoutPopularEvent from '../components/pages/Landing/CheckoutPopularEvent';
+import ExploreBestShow from "../components/pages/Landing/ExploreBestShow";
+import UpcomingEvents from "../components/pages/Landing/UpcomingEvents";
+import CheckoutPopularEvent from "../components/pages/Landing/CheckoutPopularEvent";
+
 const Home: NextPageWithLayout = () => {
-    const hello = "sdfsa";
-
-    return (
-        <div className="min-h-screen text-size1Mob md:text-size1  w-full bg-bgColor2 text-textColor">
-            <LandingBanner />
-            <UpcomingEvents/>
-            <ExploreBestShow/>
-            <CheckoutPopularEvent />
-            {/* <NextEventsSection />
-            <Testimonials/> */} 
-        </div>
-    );
+  return (
+    <div className="min-h-screen text-size1Mob md:text-size1  w-full bg-bgColor2 text-textColor">
+      <LandingBanner />
+      <UpcomingEvents />
+      <ExploreBestShow />
+      <CheckoutPopularEvent />
+      {/* <NextEventsSection />
+            <Testimonials/> */}
+    </div>
+  );
 };
 
 // Home.getLayout = (children) => {
